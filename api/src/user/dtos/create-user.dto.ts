d1dd56c03148359cdc41c7ba8f3c39f5c921ec30
RandomIv <ivanishin.alexander@lll.kpi.ua>
2025-06-03 17:10:58 +0300

    feat: add needed dto and type for createUser function

---
+++ api/src/user/dtos/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Prisma } from 'generated/prisma';
+export class CreateUserDTO implements Prisma.UserCreateInput {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
---
+++ api/src/user/types/user-nopass.type.ts
@@ -0,0 +1,2 @@
+import { User } from 'generated/prisma';
+export type UserWithoutPassword = Omit<User, 'password'>;
