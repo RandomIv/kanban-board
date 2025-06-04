c69a84d2fe97d68fd20fef6206dd5c23b4ec8191
RandomIv <ivanishin.alexander@lll.kpi.ua>
2025-06-04 15:28:42 +0300

    feat: add passport related things to auth module

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { UserService } from '../user/user.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -17,8 +19,9 @@ import { JwtModule } from '@nestjs/jwt';
       }),
       inject: [ConfigService],
     }),
+    PassportModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService, PrismaService],
+  providers: [AuthService, UserService, PrismaService, LocalStrategy],
 })
 export class AuthModule {}
